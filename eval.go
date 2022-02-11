82f8715efc1bb08a0d2b6bdd27529069a6b03497
jiro4989 <jiroron666@gmail.com>
2022-02-11 14:06:19 +0900

    引数に関数を渡すとクラッシュするのを修正

--- eval.go
+++ eval.go
@@ -5,6 +5,10 @@ import (
 	"github.com/jiro4989/xlsxlang/token"
 )
 
+type Eval struct {
+	tokens []token.Token
+}
+
 func Evaluate(tokens []token.Token) token.Token {
 	for 0 < len(tokens) {
 		var t token.Token
@@ -26,6 +30,8 @@ func Evaluate(tokens []token.Token) token.Token {
 				var a, b token.Token
 				a, tokens = dequeue(tokens)
 				b, tokens = dequeue(tokens)
+				a = Evaluate([]token.Token{a})
+				b = Evaluate([]token.Token{b})
 				return f(a, b)
 			}
 		case token.KindList:
